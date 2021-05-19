430fdbe4dabcb28a75d074959e5cd2eb0371bd8a
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-05-19 14:38:20 +0900

    yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::GreekPhilosophers.name,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: Restaurant::CATEGORIES.sample
+  )
+  restaurant.save!
+
+
+end
+
+puts "Done!"
